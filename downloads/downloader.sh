5f7d46dc11d00b22cb9582857b1d8af78c3ef470
Magda <magda.jaworowska93@gmail.com>
2016-12-17 22:36:09 +0100

    Komentarze

--- downloads/downloader.sh
+++ downloads/downloader.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# skrypt pobiera pliki binarne .dat z wykresami.
+# pliki mają nazwy od 20011 do 20651 więc skrypt pobiera pliki 20011, 20021, 20031, itd
+# aktualnie jest ustawiony na pobieranie 2 plików co można zmienić.
+# uruchamiamy: ./downloader.sh
+
 # uncomment below line for full download 
-#for i in `seq 20011 10 30801`
+#for i in `seq 20011 10 20651`
 
 for i in `seq 20011 10 20021`
 	do	
