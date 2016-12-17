58bd5f499a178182f7add5a9dca5864bc89009c0
Magda <magda.jaworowska93@gmail.com>
2016-12-17 22:30:25 +0100

    fix

--- downloads/downloader.sh
+++ downloads/downloader.sh
@@ -1,4 +1,22 @@
-for i in {2002..2006}
-	do
+#!/bin/sh
+
+# uncomment below line for full download 
+#for i in `seq 20011 10 30801`
+
+for i in `seq 20011 10 20021`
+	do	
+	case "$(uname -s)" in
+
+   Darwin)
 	curl -o "plot${i}.dat" "https://physionet.org/physiobank/database/ltstdb/s${i}.dat"	
+     ;;
+
+   Linux)
+     wget "https://physionet.org/physiobank/database/ltstdb/s${i}.dat"
+     ;;
+
+   CYGWIN*|MINGW32*|MSYS*)
+     echo 'You are running MS Windows - download files manually'
+     ;;
+	esac
 	done
